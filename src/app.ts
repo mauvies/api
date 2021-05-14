dd19b5721a2a4982cda22eca0cc17c648769f133
Mauricio Viera <mauricio.viera@trioteca.com>
2021-05-14 19:41:47 +0200

    sets up server

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+import config from './config/default';
+
+const app = express();
+
+app.set('port', config.PORT);
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+export default app;
