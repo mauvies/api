4a53612ea09dea83a01edfabe4e3c6499d1a6de1
Mauricio Viera <mauricio.viera@trioteca.com>
2021-05-15 22:15:14 +0200

    creates repos basic routes and controllers

--- src/components/repos/repos.controller.ts
+++ src/components/repos/repos.controller.ts
@@ -1,17 +1,5 @@
 import { RequestHandler } from 'express';
 
-export const addRepo: RequestHandler = async (req, res) => {
-  res.send('Add repo');
-};
-
-export const getRepo: RequestHandler = async (req, res) => {
-  res.send('Should check if repo is favorite');
-};
-
-export const deleteRepo: RequestHandler = async (req, res) => {
-  res.send('Delete repo');
-};
-
 export const getRepos: RequestHandler = async (req, res) => {
-  res.send('Return all repos');
+  res.status(200).json({ msg: 'List of all owner repos general info' });
 };
--- src/components/repos/repos.routes.ts
+++ src/components/repos/repos.routes.ts
@@ -3,12 +3,6 @@ import * as reposCtrl from './repos.controller';
 
 const router = Router();
 
-router.get('/:id', reposCtrl.getRepo);
-
-router.post('/', reposCtrl.addRepo);
-
-router.delete('/:id', reposCtrl.deleteRepo);
-
 router.get('/', reposCtrl.getRepos);
 
 export default router;
