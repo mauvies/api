645f139cfcf62932ecf8b4f06f9687cb947c3ab1
Mauricio Viera <mauricio.viera@trioteca.com>
2021-05-20 23:44:38 +0200

    changes config file

--- src/config/default.ts
+++ src/config/default.ts
@@ -3,10 +3,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  API_URL: process.env.API_URL || '',
+  API_URL: process.env.API_URL || 'https://api.github.com',
   MONGO_DATABASE: process.env.MONGO_DATABASE || 'repo-zoom',
   MONGO_USER: process.env.MONGO_USER || 'admin',
   MONGO_PASSWORD: process.env.MONGO_PASSWORD || 'admin',
-  MONGO_HOST: process.env.MONGO_HOST || 'localhost',
-  PORT: process.env.PORT || 3000,
+  MONGO_HOST:
+    process.env.MONGO_HOST || process.env.NODE_ENV === 'dev'
+      ? 'localhost'
+      : 'mongodb',
+  MONGO_PORT:
+    process.env.MONGO_HOST || process.env.NODE_ENV === 'dev' ? 27017 : 27015,
+  PORT: process.env.PORT || 5000,
 };
